d355a33858bd47a0b7c34776510c6998af24080d
Jack Zhang <576411472@qq.com>
2019-01-16 16:16:48 +0800

    Update 数据生成器.py

--- 数据生成器.py
+++ 数据生成器.py
@@ -3,12 +3,15 @@ import random
 def random_points():
 	for i in range(1000):
 		a=random.uniform(1,99)
-		b=random.uniform(1,99)
-		if a<5*b+2:
-			c=1
+		b=random.uniform(-10,10)
+		c=random.uniform(-5,5)
+		d=random.uniform(-50,50)
+		if a<c*b+d+2:
+			label=0
 		else:
-			c=0
-		print("%.1f,%.1f,%d"%(a,b,c))
+			label=1
+		print("%.1f,%.1f,%.1f,%.1f,%d"%(a,b,c,d,label))
+
 
 def add_weight_for_train():
 	m=5
